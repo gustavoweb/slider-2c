435759037a501dffe77e038c5bab3501e4300def
gustavoweb <contato@gustavoweb.com>
2025-09-03 11:46:41 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,27 @@ function App() {
           <iframe src="https://www.youtube.com/embed/p7cDN1ofIw0?autoplay=1&mute=1&controls=0&loop=1&playlist=p7cDN1ofIw0&modestbranding=1&showinfo=0" frameborder="0" allow="autoplay; fullscreen"></iframe>
 
         </section>
-        <footer></footer>
+        
+        <footer>
+
+          <img src="" alt="" className="logo-footer" />
+
+          <div className="social-icons">
+            <div className="icon">
+              <img src="" alt="" id="instagram" />
+            </div>
+            <div className="icon">
+              <img src="" alt="" id="discord" />
+            </div>
+            <div className="icon">
+              <img src="" alt="" id="x" />
+            </div>
+            <div className="icon">
+              <img src="" alt="" id="youtube" />
+            </div>
+          </div>
+
+        </footer>
       </main>
     </>
   )
